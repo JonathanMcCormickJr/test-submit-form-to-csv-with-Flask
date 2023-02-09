6a53f97fe610baa1c80670690f8d03bba5ccc372
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-09 17:36:12 +0000

    Hmm. This version does not work...

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__)
 
+app.debug = True
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        user_text = request.form["text"]
+        user_text = request.form["name_input"]
 
         with open("text.csv", "a", newline="") as f:
             writer = csv.writer(f)
             writer.writerow([user_text])
 
-        return "Text saved!"
+        return "Name saved!"
 
-    return """
-        <form method="post">
-            Input text: <input type="text" name="text">
-            <input type="submit" value="Submit">
-        </form>
-    """
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
